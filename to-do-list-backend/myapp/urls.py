089cef4be2afffaf5e7d0fdf5b9d29effc56af07
Claire <claire.mb0504@gmail.com>
2025-04-04 13:24:53 +0800

    Changes urls.py

--- to-do-list-backend/myapp/urls.py
+++ to-do-list-backend/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
-    path('fetch', TodoListView.as_view(), name='todo-fetch'),  # Fetch tasks endpoint
-    path('create', TodoCreateView.as_view(), name='todo-create'),  # Create a new task
-    path('<int:pk>/update', TodoUpdateView.as_view(), name='todo-update'),  # Update a specific task
-    path('<int:pk>/delete', TodoDeleteView.as_view(), name='todo-delete'),  # Delete a specific task
+    path('fetch/', TodoListView.as_view(), name='todo-fetch'),  
+    path('create/', TodoCreateView.as_view(), name='todo-create'),
+    path('<int:pk>/update/', TodoUpdateView.as_view(), name='todo-update'),
+    path('<int:pk>/delete/', TodoDeleteView.as_view(), name='todo-delete'),
 ]
