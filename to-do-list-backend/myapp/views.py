80fc6f7abc7e3989a4c9639fb724d9fbbaf11cc6
Claire <claire.mb0504@gmail.com>
2025-04-04 13:42:08 +0800

    Changes views

--- to-do-list-backend/myapp/views.py
+++ to-do-list-backend/myapp/views.py
@@ -1,30 +1,32 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from .models import TodoItem
 from .serializers import TodoItemSerializer
 
-# View to fetch all To-Do items
+
 class TodoListView(APIView):
     def get(self, request, *args, **kwargs):
-        todos = TodoItem.objects.all()  # Get all To-Do items
+        todos = TodoItem.objects.all()
         serializer = TodoItemSerializer(todos, many=True)
         return Response(serializer.data)
 
-# View to create a new To-Do item
+@method_decorator(csrf_exempt, name="dispatch")
 class TodoCreateView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = TodoItemSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Save new To-Do item
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# View to retrieve, update or delete a specific To-Do item
+@method_decorator(csrf_exempt, name="dispatch")
 class TodoDetailView(APIView):
     def get(self, request, pk, *args, **kwargs):
         try:
-            todo = TodoItem.objects.get(pk=pk)  # Get To-Do item by pk
+            todo = TodoItem.objects.get(pk=pk)
         except TodoItem.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
@@ -33,26 +35,26 @@ class TodoDetailView(APIView):
 
     def put(self, request, pk, *args, **kwargs):
         try:
-            todo = TodoItem.objects.get(pk=pk)  # Get To-Do item by pk
+            todo = TodoItem.objects.get(pk=pk)
         except TodoItem.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = TodoItemSerializer(todo, data=request.data)  # Update data
+        serializer = TodoItemSerializer(todo, data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Save updated To-Do item
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, *args, **kwargs):
         try:
-            todo = TodoItem.objects.get(pk=pk)  # Get To-Do item by pk
+            todo = TodoItem.objects.get(pk=pk)
         except TodoItem.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        todo.delete()  # Delete To-Do item
+        todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-# View to update a specific To-Do item (can be part of TodoDetailView, or separate)
+@method_decorator(csrf_exempt, name="dispatch")
 class TodoUpdateView(APIView):
     def put(self, request, pk, *args, **kwargs):
         try:
@@ -62,11 +64,11 @@ class TodoUpdateView(APIView):
 
         serializer = TodoItemSerializer(todo, data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Save updated To-Do item
+            serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# View to delete a specific To-Do item (can be part of TodoDetailView, or separate)
+@method_decorator(csrf_exempt, name="dispatch")
 class TodoDeleteView(APIView):
     def delete(self, request, pk, *args, **kwargs):
         try:
@@ -74,5 +76,5 @@ class TodoDeleteView(APIView):
         except TodoItem.DoesNotExist:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        todo.delete()  # Delete To-Do item
+        todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
