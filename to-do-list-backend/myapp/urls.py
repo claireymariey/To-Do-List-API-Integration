b4ced34eeafefa16ce1b79575b66880bbd7ebf42
Claire <claire.mb0504@gmail.com>
2025-04-10 13:38:05 +0800

    Backend changes

--- to-do-list-backend/myapp/urls.py
+++ to-do-list-backend/myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
+from rest_framework.authtoken.views import obtain_auth_token
+from myapp.views import MyProtectedView
 
 urlpatterns = [
     path('fetch/', TodoListView.as_view(), name='todo-list'),
     path('create/', TodoCreateView.as_view(), name='todo-create'),
     path('<int:pk>/update/', TodoUpdateView.as_view(), name='todo-update'),
     path('<int:pk>/delete/', TodoDeleteView.as_view(), name='todo-delete'),
+    path('api/protected/', MyProtectedView.as_view(),name='protected-view'),
+    path('api/token/', obtain_auth_token, name='token-obtain'),
+
 ]
