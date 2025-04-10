5f8a03f195c2700699f3358c6b4aa6aa8b36e0ed
Claire <claire.mb0504@gmail.com>
2025-04-10 19:21:32 +0800

    Authentication Added

--- to-do-list-backend/myapp/urls.py
+++ to-do-list-backend/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 from myapp.views import MyProtectedView
+from myapp.views import SecureHelloView
 
 urlpatterns = [
     path('fetch/', TodoListView.as_view(), name='todo-list'),
@@ -8,5 +9,6 @@ urlpatterns = [
     path('<int:pk>/update/', TodoUpdateView.as_view(), name='todo-update'),
     path('<int:pk>/delete/', TodoDeleteView.as_view(), name='todo-delete'),
     path('api/protected/', MyProtectedView.as_view(),name='protected-view'),
+    path('secure-hello/', SecureHelloView.as_view()),
 
 ]
